f6033d9db8db1b4f9ff08bfc15d7a19b4cae74ef
Henry Erich III <Henry Erich III>
2016-11-19 10:28:01 -0500

    Add: angular factory generator

---
+++ gg-factory.js
@@ -0,0 +1,56 @@
+var program = require('commander');
+var fs = require('fs');
+
+String.prototype.toDash = function () {
+    return this.replace(/([A-Z])/g, function ($1) { return "-" + $1.toLowerCase(); });
+};
+
+String.prototype.upperFirst = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+String.prototype.lowerFirst = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+}
+
+program
+    .version('0.0.1')
+    .usage("[options] <name>")
+    .action(function (name) {
+        console.log('Generating:');
+        const appRoot = "src/app/";
+
+        name = name.replace("Factory","");
+        name = name.lowerFirst();
+
+        createFactory("", name);
+    })
+    .parse(process.argv);
+
+
+
+
+function createFactory(path, name) {
+    console.log('  - ' + path + name + "Factory.js");
+
+    fs.writeFile(
+        path + name + "Factory.js",
+        `(function(){
+    "use strict";
+    
+    angular
+        .module("app")
+        .factory("${name}Factory", factory);
+
+    function factory($http) {
+        return {
+            getAll${name.upperFirst()}s: () => $http.get("")
+        };
+    }
+    controller.$inject = ["$http"];
+    
+})()`,
+        function (error) {
+            if (error) console.log("error creating factory", error);
+        });
+}
--- gg.js
+++ gg.js
@@ -3,6 +3,7 @@ var program = require('commander');
 program
   .version('0.0.1')
   .command("echo <value>", "Echo back whatever value is passed.")
-  .command("component <name>", "Example of code generation. Creates an Angular component.")
+  .command("component <name>", "Creates an Angular component.")
+  .command("factory <name>", "Creates an Angular factory.")
   .parse(process.argv);
   
