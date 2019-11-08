87fc6b22a894db3f7549876389f2b4c721c57981
Brian Hinken <bhinken@streamlinedb.com>
2019-11-08 15:35:35 -0500

    added --es6 (-6) and --project (-p) for modal

--- gg-modal.js
+++ gg-modal.js
@@ -6,9 +6,15 @@ String.prototype.toDash = function () {
     return this.replace(/([A-Z])/g, function ($1) { return "-" + $1.toLowerCase(); });
 };
 
+String.prototype.upperFirst = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 program
     .version('0.0.1')
     .usage("[options] <name>")
+    .option("-6, --es6", "Generate component in ES6.")
+    .option("-p, --project", "Add generated files to .csproj file.")
     .action(function (name) {
         console.log('Generating:');
 
@@ -34,14 +40,25 @@ program
 
         const templatePath = name + "\\" + name + ".html";
         //console.log("templatePath:" + templatePath);
-       
+
         let templateUrl = pathBackToRoot.replace(/\\/g, "/") + "/" + name + ".html";
         if(templateUrl.startsWith("\\"))
             templateUrl = templateUrl.substring(1);
-        console.log("templateUrl:" + templateUrl);
+        //console.log("templateUrl:" + templateUrl);
 
         createTemplate(templatePath, name);
-        createComponent(componentPath, name, templateUrl);
+
+        if (program.es6) {
+            createES6Component(componentPath, name, templateUrl);
+        } else {
+            createComponent(componentPath, name, templateUrl);
+        }
+
+        if (program.project) {
+            const projectFilePath = fullPackageDirPath + "\\" + fs.readdirSync(fullPackageDirPath).find(fileName => fileName.match(/.*\.csproj/ig));
+            //console.log(`projectFilePath: ${projectFilePath}`);
+            addFilesToProject(projectFilePath, [templatePath, componentPath], pathBackToRoot);
+        }
     })
     .parse(process.argv);
 
@@ -111,6 +128,61 @@ function createComponent(componentPath, name, templateUrl) {
         });
 }
 
+function createES6Component(componentPath, name, templateUrl) {
+    console.log('  - ' + componentPath);
+
+    fs.writeFile(
+        componentPath,
+`class ${name.upperFirst()}Controller {
+    static $inject = ["stToast"];
+
+    constructor(stToast) {
+        this.toast = stToast;
+    }
+
+    $onInit() {
+        angular.extend(this, this.resolve); // copy any resolved properties onto the controller
+    }
+
+    save = () => {
+        // return something to the modal opener
+        this.close({ $value: this.someResult });
+    };
+
+    /*
+    // to open the modal (copy to some other controller)
+    openModal = () => {
+        this.$uibModal.open({
+                component: "${name}",
+                resolve: {
+                    someValue: () => this.someValue
+                }
+            }).result
+            .then(someResult => {
+                console.log(someResult);
+            })
+            .catch(angular.noop); // suppress 'Possibly unhandled rejection' message in console, or handle the modal being dismissed some other way
+    };
+    */
+}
+
+angular
+    .module("app")
+    .component("${name}", { // ${name.toDash()}
+        templateUrl: "${templateUrl}",
+        controller: ${name.upperFirst()}Controller,
+        bindings: {
+            resolve: "<",
+            close: "&",
+            dismiss: "&"
+        }
+    });
+`,
+        function (error) {
+            if (error) console.log("error creating component", error);
+        });
+}
+
 function createTemplate(templatePath, name) {
     console.log('  - ' + templatePath);
     fs.writeFile(
@@ -122,7 +194,7 @@ function createTemplate(templatePath, name) {
 <div class="modal-body">
 </div>
 <div class="modal-footer">
-    <button st-btn-type="cancel" ng-click="$ctrl.cancel()"></button>
+    <button st-btn-type="cancel" ng-click="$ctrl.dismiss()"></button>
     <button st-btn-type="save" ng-click="$ctrl.save()"></button>
 </div>
 `,
@@ -131,3 +203,21 @@ function createTemplate(templatePath, name) {
         });
 }
 
+function addFilesToProject(projectFilePath, filesToAdd, pathBackToRoot) {
+    console.log('  - Add files to project');
+    fs.readFile(projectFilePath, function (err, data) {
+        if (err) throw err;
+        const lines = data.toString().split("\r\n");
+        const lineIdx = lines.findIndex(line => line.match(/package\.json/ig));
+        if (lineIdx >= 0) {
+            filesToAdd.forEach(fileToAdd => {
+                lines.splice(lineIdx, 0, `    <None Include="${pathBackToRoot}\\${path.basename(fileToAdd)}" />`);
+            });
+        }
+        var file = fs.createWriteStream(projectFilePath);
+        file.on('error', function (err) { console.log("error writing to " + projectFilePath, err); });
+        lines.forEach(function (v) { file.write(v + '\r\n'); });
+        file.end();
+    });
+
+}
